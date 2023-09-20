e396ba8d2acd0feb60c599f9fd9a688fd616055b
zenDev-2 <cfk102@gmail.com>
2023-09-20 02:58:46 -0400

    added: expandedClass writable

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,3 +2,6 @@ import { writable } from 'svelte/store';
 import type { Citation } from '../ts/types';
 export const scrapes = writable<Citation[]>([]);
 export const urlHistory = writable<string[]>([]);
+
+// css class used to expand the CitationDisplay to full width. used on line 114 of Fetcher.svelte
+export const expandedClass = writable('');
