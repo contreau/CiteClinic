5b77cdf75a185adcb34bf7bdea4e773d51f1fa9a
zenDev-2 <cfk102@gmail.com>
2023-10-12 03:02:41 -0400

    improved StyleLayer

--- src/lib/StyleLayer.svelte
+++ src/lib/StyleLayer.svelte
@@ -9,7 +9,7 @@
 		await import('vanilla-colorful');
 	});
 
-	let color = '#1e88e5';
+	let color = '#000000';
 
 	function handleColorChanged(event: any) {
 		color = event.detail.value;
@@ -58,7 +58,7 @@
 		</p>
 	</div>
 	<button
-		class="style-dropdown"
+		class="btn-grad"
 		on:click={() => {
 			styleDropdownVisible = !styleDropdownVisible;
 		}}>{optionsText}</button
@@ -69,9 +69,16 @@
 			in:fly={{ x: -200, duration: 650 }}
 			out:fade={{ duration: 200 }}
 		>
+			<p class="style-option-title"><b>Border Thickness </b></p>
+
+			<p class="style-option-title"><b>Border Color </b></p>
+
+			<p class="style-option-title"><b>Shadow Type </b></p>
+
+			<hr />
+
 			<div class="border-thickness grid-item">
-				<p><b>Border Thickness </b></p>
-				<p>{borderThickness}px</p>
+				<p class="border-value-text">{borderThickness}px</p>
 				<input
 					type="range"
 					bind:value={borderThickness}
@@ -84,20 +91,31 @@
 				/>
 			</div>
 			<div class="border-color grid-item">
-				<output><p><b>Border Color: </b>{color}</p></output>
+				<output class="color-value-text">{color}</output>
 				<hex-color-picker {color} on:color-changed={handleColorChanged} />
 			</div>
 
-			<div class="style-input shadow-control grid-item">
-				<p><b>Shadow Type</b></p>
-				<input name="shadow-options" id="shadow1" type="radio" />
-				<label for="shadow1">Shadow 1</label>
-				<br />
-				<input name="shadow-options" id="shadow2" type="radio" />
-				<label for="shadow2">Shadow 2</label>
-				<br />
-				<input name="shadow-options" id="shadow3" type="radio" />
-				<label for="shadow3">Shadow 3</label>
+			<div class="shadow-control grid-item">
+				<div class="radio-input-block">
+					<div>
+						<input name="shadow-options" id="shadow1" type="radio" />
+						<label for="shadow1">Shadow 1</label>
+					</div>
+					<br />
+					<div>
+						<input name="shadow-options" id="shadow2" type="radio" />
+						<label for="shadow2">Shadow 2</label>
+					</div>
+					<br />
+					<div>
+						<input name="shadow-options" id="shadow3" type="radio" />
+						<label for="shadow3">Shadow 3</label>
+					</div>
+					<div>
+						<input name="shadow-options" id="noshadow" type="radio" />
+						<label for="noshadow">None</label>
+					</div>
+				</div>
 			</div>
 		</div>
 	{/if}
@@ -119,6 +137,7 @@
 		animation: fadeIn 0.4s ease-in forwards;
 
 		h3 {
+			font-size: 1.5rem;
 			text-align: center;
 			max-width: 75%;
 			margin: 0 auto;
@@ -162,34 +181,42 @@
 		}
 	}
 
-	button.style-dropdown {
-		display: block;
-		margin: 0 auto;
+	.btn-grad {
+		background-image: linear-gradient(to right, #1a2980 0%, #0bb6b3 51%, #1a2980 100%);
 		font-size: inherit;
-		padding: 0.4em 1em;
-		border-radius: 10px;
+		min-width: 160px;
 		border: solid 2px transparent;
-		transition: all 0.3s;
-		background-color: #2b2a33;
+		margin: 0 auto;
+		padding: 0.4em 1em;
+		transition: 0.5s;
+		background-size: 200% auto;
+		color: white;
 		cursor: pointer;
-		&:hover,
-		&:focus-visible {
-			outline: transparent;
-			background-color: #474747;
-		}
+		border-radius: 10px;
+		display: block;
+	}
+
+	.btn-grad:hover {
+		background-position: right center; /* change the direction of the change here */
+		color: #fff;
+		text-decoration: none;
 	}
 
 	.style-options-wrap {
+		background-color: #1e1e1ed2;
+		border-radius: 15px;
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
 		justify-content: center;
 		margin: 0 auto;
-		padding-bottom: 2em;
-		padding-top: 1em;
+		margin-top: 1rem;
+		padding-bottom: 1.5em;
+		padding-top: 0.8em;
 		// align-items: center;
 
 		.grid-item {
 			justify-self: center;
+			place-self: center;
 		}
 	}
 
@@ -227,6 +254,53 @@
 		}
 	}
 
+	hr {
+		grid-column: span 3;
+		margin-top: 0;
+		margin-bottom: 0.5rem;
+		width: 100%;
+		border-radius: 15px;
+		color: #4a4a4a;
+	}
+
+	.style-option-title {
+		font-size: 1.5rem;
+		color: #56a8ff;
+		text-align: center;
+		margin-top: 0;
+		margin-bottom: 0.8rem;
+	}
+
+	.border-thickness {
+		.border-value-text {
+			text-align: center;
+			font-size: 1.35rem;
+		}
+
+		input {
+			display: block;
+			margin: 0 auto;
+		}
+	}
+
+	.color-value-text {
+		display: block;
+		text-align: center;
+		font-size: 1.35rem;
+	}
+
+	.radio-input-block {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		div {
+			min-width: 127px;
+			label {
+				font-size: 1.25rem;
+			}
+		}
+	}
+
 	// Color Picker
 	hex-color-picker {
 		width: 150px;
@@ -253,6 +327,12 @@
 
 		.style-options-wrap {
 			grid-template-columns: 1fr;
+			gap: 2rem;
+		}
+
+		.style-option-title,
+		hr {
+			display: none;
 		}
 	}
 </style>
