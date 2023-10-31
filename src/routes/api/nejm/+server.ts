06a42a847001c604a9e448c217758e275f113758
zenDev-2 <cfk102@gmail.com>
2023-10-31 00:22:17 -0400

    puppeteer maintenance

--- src/routes/api/nejm/+server.ts
+++ src/routes/api/nejm/+server.ts
@@ -3,33 +3,33 @@ import { json, error } from '@sveltejs/kit';
 import { nejmPARAMS } from '$lib/parameters';
 import { retrieve } from '../../../ts/serverFunctions.js';
 import chromium from '@sparticuz/chromium-min';
-import puppeteer from 'puppeteer-core';
-// import 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
-// import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi';
-// import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
-// import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime';
-// import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs';
-// import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow';
-// import 'puppeteer-extra-plugin-stealth/evasions/media.codecs';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor';
-// import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver';
-// import 'puppeteer-extra-plugin-stealth/evasions/sourceurl';
-// import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override';
-// import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor';
-// import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions';
-// import 'puppeteer-extra-plugin-user-preferences';
-// import 'puppeteer-extra-plugin-user-data-dir';
-// import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import puppeteer from 'puppeteer-extra';
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.app';
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.csi';
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes';
+import 'puppeteer-extra-plugin-stealth/evasions/chrome.runtime';
+import 'puppeteer-extra-plugin-stealth/evasions/defaultArgs';
+import 'puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow';
+import 'puppeteer-extra-plugin-stealth/evasions/media.codecs';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.languages';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.permissions';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.plugins';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.vendor';
+import 'puppeteer-extra-plugin-stealth/evasions/navigator.webdriver';
+import 'puppeteer-extra-plugin-stealth/evasions/sourceurl';
+import 'puppeteer-extra-plugin-stealth/evasions/user-agent-override';
+import 'puppeteer-extra-plugin-stealth/evasions/webgl.vendor';
+import 'puppeteer-extra-plugin-stealth/evasions/window.outerdimensions';
+import 'puppeteer-extra-plugin-user-preferences';
+import 'puppeteer-extra-plugin-user-data-dir';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import type { Citation } from '../../../ts/types.js';
 
 // chromium.setHeadlessMode = 'new';
 // chromium.setGraphicsMode = false;
 
-// puppeteer.use(StealthPlugin());
+puppeteer.use(StealthPlugin());
 
 function formatName(name: string) {
 	let parts: string[] | string;
@@ -73,8 +73,6 @@ export async function GET({ url }) {
 			const page = await browser.newPage();
 
 			await page.goto(target);
-			const precontent = await page.content();
-			console.log(precontent);
 			await page.waitForSelector('.m-article-header__authors', {
 				visible: true,
 				timeout: 3000
